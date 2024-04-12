bb2d52efad0eb966061c6be7eb2a9a47787b7d6e
Md Samer Ansari <smaitykhan@gmail.com>
2024-04-12 09:02:22 +0530

    check committing is verified

--- week-3/01-middlewares/03-errorcount.js
+++ week-3/01-middlewares/03-errorcount.js
@@ -1,14 +1,8 @@
-const request = require('supertest');
-const assert = require('assert');
 const express = require('express');
 
 const app = express();
 let errorCount = 0;
 
-// You have been given an express server which has a few endpoints.
-// Your task is to
-// 1. Ensure that if there is ever an exception, the end user sees a status code of 404
-// 2. Maintain the errorCount variable whose value should go up every time there is an exception in any endpoint
 
 app.get('/user', function(req, res) {
   throw new Error("User not found");
@@ -22,5 +16,15 @@ app.post('/user', function(req, res) {
 app.get('/errorCount', function(req, res) {
   res.status(200).json({ errorCount });
 });
+// error middleware
+app.use((err, req, res, next) => {
+  res.status(404).send({})
+  errorCount = errorCount + 1;
+});
 
 module.exports = app;
+
+// You have been given an express server which has a few endpoints.
+// Your task is to
+// 1. Ensure that if there is ever an exception, the end user sees a status code of 404
+// 2. Maintain the errorCount variable whose value should go up every time there is an exception in any endpoint
